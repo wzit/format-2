8006b929ecea48493e436f422038c138ca343ec7
justinls <justinshrake@gmail.com>
2013-08-18 09:49:00 -0700

    place qualifier by identifier not type

--- format.hpp
+++ format.hpp
@@ -46,7 +46,7 @@ inline void format_helper(size_t arg_index, std::string &str, T &&val, Args &&..
 namespace ext {
 
 template <typename... Args>
-inline const char *format(const char *s, Args&&... args) {
+inline const char *format(const char *s, Args &&...args) {
   std::string str{s};
   format_helper(1, str, std::forward<Args>(args)...);
   return str.c_str();
